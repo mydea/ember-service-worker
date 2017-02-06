20ba75a5a44bb023d86e62d683c6a8b7a1ab41a3
Marten Schilstra <marten.schilstra@dockyard.com>
2017-02-06 09:28:55 +0100

    WIP

--- node-tests/acceptance-test.js
+++ node-tests/acceptance-test.js
@@ -2,15 +2,18 @@ var assert = require('chai').assert;
 var fs = require('fs');
 var path = require('path');
 var rimraf = require('rimraf').sync;
-var spawnSync = require('child_process').spawnSync;
+var childProcess = require('child_process');
+
+var spawn = childProcess.spawn;
+var spawnSync = childProcess.spawnSync;
 
 var emberCLIPath = path.resolve(__dirname, './fixtures/simple-app/node_modules/ember-cli/bin/ember');
 
 describe('Acceptance Tests', function() {
   this.timeout(120000);
+  var fixturePath = path.resolve(__dirname, './fixtures/simple-app');
 
   context('A Simple App', function() {
-    var fixturePath = path.resolve(__dirname, './fixtures/simple-app');
 
     function dist(file) {
       return path.join(fixturePath, 'dist', file);
@@ -41,6 +44,38 @@ describe('Acceptance Tests', function() {
       contains(dist('sw-registration.js'), "self.hello = 'Hello from Ember Service Worker Test';");
     });
   });
+
+  context('SW', function() {
+    let results;
+    let emberServer;
+
+    before(function(done) {
+      emberServer = runEmberCommandAsync(fixturePath, ['server', '--port', '4242']);
+
+      emberServer.stdout.on('data', (data) => {
+        if (data.indexOf('Build successful') !== -1) {
+          spawnSync('lighthouse', [
+            '--output', 'json',
+            '--output-path', `${__dirname}/lighthouse-results.json`,
+            '--config-path', `${__dirname}/fixtures/lighthouse-config.json`,
+            'http://localhost:4242']);
+
+          results = require('./lighthouse-results.json');
+          done();
+        }
+      });
+    });
+
+    after(function() {
+      emberServer.kill();
+      fs.unlinkSync(`${__dirname}/lighthouse-results.json`);
+      cleanup(fixturePath);
+    });
+
+    it('is registered', function() {
+      assert.ok(results.audits['service-worker'].score);
+    });
+  });
 });
 
 function runEmberCommand(packagePath, command) {
@@ -51,6 +86,12 @@ function runEmberCommand(packagePath, command) {
   if (result.status !== 0) {
     throw new Error(result.stderr.toString());
   }
+}
+
+function runEmberCommandAsync(packagePath, command) {
+  return spawn(emberCLIPath, command, {
+    cwd: packagePath
+  });
 }
 
 function cleanup(packagePath) {
